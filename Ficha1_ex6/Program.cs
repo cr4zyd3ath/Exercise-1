e2f463decb2ab371866de18e70e2f0b284311607
cr4zy_d3ath <58996762+cr4zyd3ath@users.noreply.github.com>
2021-12-24 05:26:16 +0000

    Add files via upload

---
+++ Ficha1_ex6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Ficha1_ex5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaração das variáveis coloquei double para calcular numeros com ou sem ,
+            double n1, n2, media;
+            //mensagem para digitar o n1
+            Console.Write("Digite o valor nota 1: ");
+            //variável r recebe o valor
+            n1 = double.Parse(Console.ReadLine());
+            //mensagem para digitar o n2
+            Console.Write("Digite o valor nota 2: ");
+            //variável r recebe o valor
+            n2 = double.Parse(Console.ReadLine());
+            //O volume recebe os dados
+            media = (n1 + n2) / 2;
+            Console.WriteLine("A media das notas: " + media);
+            Console.WriteLine();
+            Console.Write("Pressione qualquer tecla para terminar . . . ");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ficha1_ex7/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Ficha1_ex7
+{
+ class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaração das variáveis
+            double nota = 0.0, soma = 0.0;
+            double media = 0.0;
+            int i;
+
+            for (i = 0; i < 6; i++)
+            {
+                Console.WriteLine("Insira o valor das notas: ");
+                nota = Convert.ToDouble(Console.ReadLine());
+
+                soma = soma + nota;
+            }
+
+            media = soma / i;
+
+            Console.WriteLine("A média das 6 notas: " + media);
+            Console.WriteLine();
+            Console.Write("Pressione qualquer tecla para terminar . . . ");
+            Console.ReadKey();
+        }
+    }
+}
